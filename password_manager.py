553c7f4acf44baa0bb70a3dc7d1e8904a51a823b
Christoph <chris24.blessing@gmail.com>
2018-08-29 13:54:13 +0200

    Added basic persistence to account information database

--- password_manager.py
+++ password_manager.py
@@ -1,25 +1,37 @@
+import pickle
 from dataclasses import dataclass
 
 
 class AccountInformationDatabase:
 
-    def __init__(self):
-        self.database = {}
+    def __init__(self, database_path='database.pickle'):
+        self.database_path = database_path
+        try:
+            with open(database_path, 'rb') as f:
+                self.database = pickle.load(f)
+        except FileNotFoundError:
+            self.database = {}
 
     def add(self, name, login, password):
         if name in self.database:
             raise ValueError(f'"{name}" already exists!')
         self.database[name] = AccountInformation(login, password)
+        self.save()
 
     def remove(self, name):
         if name not in self.database:
             raise ValueError(f'"{name}" does not exist!')
         del self.database[name]
+        self.save()
 
     def get(self, name):
         if name not in self.database:
             raise ValueError(f'"{name}" does not exist!')
         return self.database[name]
+
+    def save(self):
+        with open(self.database_path, 'wb') as f:
+            pickle.dump(self.database, f)
 
 
 @dataclass
@@ -30,9 +42,8 @@ class AccountInformation:
 
 def main():
     account_information_database = AccountInformationDatabase()
-    account_information_database.add('hallo', 'test@web.de', 'password123')
-    account_information_database.remove('test')
-    print(account_information_database.get('test'))
+    account_information_database.remove('hallo')
+    print(account_information_database.get('hallo'))
 
 
 if __name__ == '__main__':
