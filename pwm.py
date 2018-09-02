5cce9dbbaf47d2934cd0fc03d5ffdc4f6efa999e
Christoph <chris24.blessing@gmail.com>
2018-09-02 13:29:45 +0200

    Removed old SitesDatabase

--- pwm.py
+++ pwm.py
@@ -19,26 +19,29 @@ class Database:
         self.class_ = class_
         self.database_path = database_path
         self.database = None
+        self.n_entries = None
         try:
             self.load()
         except FileNotFoundError:
             self.init()
 
     def get(self, name):
-        if name not in self.database:
-            raise DoesNotExist
-        return self.database[name]
+        if name not in self.database['data']:
+            raise DoesNotExistError
+        return self.database['data'][name]
 
     def new(self, name, instance):
-        if name in self.database:
-            raise AlreadyExists
+        if name in self.database['data']:
+            raise AlreadyExistsError
         self.database['data'][name] = instance
+        self.update_n_entries()
         self.save()
 
     def remove(self, name):
-        if name not in self.database:
-            raise DoesNotExist
+        if name not in self.database['data']:
+            raise DoesNotExistError
         del self.database['data'][name]
+        self.update_n_entries()
         self.save()
 
     def save(self):
@@ -49,81 +52,41 @@ class Database:
         with open(self.database_path, 'r') as f:
             database_as_dict = json.load(f)
             if database_as_dict['class'] != self.class_.__name__:
-                raise DatabaseClassMismatch
+                raise DatabaseClassMismatchError
             # Convert the dicts back to dataclass objects
-            self.database = {}
+            self.database = {'class': database_as_dict['class'], 'data': {}}
             for key, object_as_dict in database_as_dict['data'].items():
-                self.database[key] = self.class_(*object_as_dict.values())
+                self.database['data'][key] = self.class_(*object_as_dict.values())
+            self.update_n_entries()
 
     def init(self):
         self.database = {'class': self.class_.__name__, 'data': {}}
+        self.update_n_entries()
         self.save()
 
     def drop(self):
-        self.database = {}
+        self.database['data'] = {}
+        self.update_n_entries()
         self.save()
 
+    def update_n_entries(self):
+        self.n_entries = len(self.database['data'])
+
     def __iter__(self):
-        for name, object_ in self.database:
+        for name, object_ in self.database['data'].items():
             yield name, object_
 
 
-class DoesNotExist(Exception):
+class DoesNotExistError(Exception):
     pass
 
 
-class AlreadyExists(Exception):
+class AlreadyExistsError(Exception):
     pass
 
 
-class DatabaseClassMismatch(Exception):
+class DatabaseClassMismatchError(Exception):
     pass
-
-
-class SiteDatabase:
-
-    def __init__(self, database_path='database.json'):
-        self.database_path = database_path
-        try:
-            self.database = self.load()
-        except FileNotFoundError:
-            self.database = {}
-        self.n_sites = len(self.database)
-
-    def add(self, name, login, password):
-        if name in self.database:
-            raise SiteAlreadyExists()
-        self.database[name] = Site(login, password)
-        self.save()
-
-    def remove(self, name):
-        if name not in self.database:
-            raise SiteDoesNotExist()
-        del self.database[name]
-        self.save()
-
-    def get(self, name):
-        if name not in self.database:
-            raise SiteDoesNotExist()
-        return self.database[name]
-
-    def save(self):
-        with open(self.database_path, 'w') as f:
-            json_database = {name: {'login': site.login, 'password': site.password} for name, site in self}
-            json.dump(json_database, f, indent=4, sort_keys=True)
-
-    def load(self):
-        with open(self.database_path, 'r') as f:
-            json_database = json.load(f)
-            return {name: Site(json_site['login'], json_site['password']) for name, json_site in json_database.items()}
-
-    def drop(self):
-        self.database = {}
-        self.save()
-
-    def __iter__(self):
-        for name, site in self.database.items():
-            yield name, site
 
 
 @dataclasses.dataclass()
@@ -132,34 +95,30 @@ class Site:
     password: str
 
 
-class SiteAlreadyExists(Exception):
-    pass
-
-
-class SiteDoesNotExist(Exception):
-    pass
-
-
 def abort_if_false(context, _, value):
     if not value:
         context.abort()
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(context):
+    database = Database('sites.json', Site)
+    context.obj = {'database': database}
 
 
 @cli.command()
 @click.option('--name', type=str, help='Name of the new site.', prompt=True)
 @click.option('--login', type=str, help='Login information of the new site.', prompt=True)
 @click.option('--password', type=str, help='Password of the new site.', prompt=True, hide_input=True)
-def new(name, login, password):
+@click.pass_context
+def new(context, name, login, password):
     """Add a new site to the database."""
-    db = SiteDatabase()
+    database = context.obj['database']
+    site = Site(login, password)
     try:
-        db.add(name, login, password)
-    except SiteAlreadyExists:
+        database.new(name, site)
+    except AlreadyExistsError:
         click.echo(f'Error: A site with the name "{name}" already exists. Please choose another name.')
     else:
         click.echo(f'Added entry with name "{name}".')
@@ -169,12 +128,13 @@ def new(name, login, password):
 @click.option('--name', type=str, help='Name of the site.', prompt=True)
 @click.option('--yes', is_flag=True, callback=abort_if_false, expose_value=False,
               prompt=f'Are you sure you want to remove the site?')
-def remove(name):
+@click.pass_context
+def remove(context, name):
     """Remove a site from the database."""
-    db = SiteDatabase()
+    database = context.obj['database']
     try:
-        db.remove(name)
-    except SiteDoesNotExist:
+        database.remove(name)
+    except DoesNotExistError:
         click.echo(f'Error: A site with the name "{name}" does not exist.')
     else:
         click.echo(f'Removed the site with the name "{name}".')
@@ -183,12 +143,13 @@ def remove(name):
 @cli.command()
 @click.option('--name', type=str, help='Name of the site.', prompt=True)
 @click.option('--login', 'copy_login', help='Copy the login instead of the password.', is_flag=True)
-def get(name, copy_login):
+@click.pass_context
+def get(context, name, copy_login):
     """Copy the password of a site to the clipboard."""
-    db = SiteDatabase()
+    database = context.obj['database']
     try:
-        site = db.get(name)
-    except SiteDoesNotExist:
+        site = database.get(name)
+    except DoesNotExistError:
         click.echo(f'Error: A site with the name "{name}" does not exist.')
     else:
         if copy_login:
@@ -202,36 +163,32 @@ def get(name, copy_login):
 @cli.command()
 @click.option('--yes', is_flag=True, callback=abort_if_false, expose_value=False,
               prompt=f'Are you sure you want to drop the database?')
-def drop():
+@click.pass_context
+def drop(context):
     """Drop the database."""
-    db = SiteDatabase()
-    db.drop()
+    database = context.obj['database']
+    database.drop()
     click.echo('Database dropped.')
 
 
 @cli.command()
-def ls():
+@click.pass_context
+def ls(context):
     """List all sites in the database."""
-    db = SiteDatabase()
-    if db.n_sites == 0:
+    database = context.obj['database']
+    if database.n_entries == 0:
         click.echo('0 sites in database.')
     else:
-        if db.n_sites == 1:
-            click.echo(f'{db.n_sites} site in database:')
+        if database.n_entries == 1:
+            click.echo(f'{database.n_entries} site in database:')
         else:
-            click.echo(f'{db.n_sites} sites in database:')
-        for name, site in db:
+            click.echo(f'{database.n_entries} sites in database:')
+        for name, site in database:
             click.echo(f'Name: {name}, Login: {site.login}, Password: {site.password}')
 
 
 def main():
-    db = SiteDatabase()
-    # db.add('Test1', 'mail1@test.de', 'password1')
-    # db.add('Test2', 'mail2@test.de', 'password2')
-    # db.add('Test3', 'mail3@test.de', 'password3')
-    # db.add('Test4', 'mail4@test.de', 'password4')
-    # db.add('Test5', 'mail5@test.de', 'password5')
-    db.drop()
+    pass
 
 
 if __name__ == '__main__':
