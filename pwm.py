8fdf9512786f5087058444474d5cca5862a80303
Christoph <chris24.blessing@gmail.com>
2018-09-07 19:48:41 +0200

    Create user method in UserDatabase

--- pwm.py
+++ pwm.py
@@ -1,8 +1,13 @@
 import sqlite3
 import os
+import base64
 from dataclasses import dataclass
 
 import click
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 class Database:
@@ -25,15 +30,6 @@ class UserDatabase(Database):
                 enc_dek text
                 )''')
 
-    def _select_user_by_name(self, name):
-        self.cursor.execute('SELECT * FROM users WHERE name = :name', {'name': name})
-
-    def _check_user_existence_by_name(self, name):
-        self._select_user_by_name(name)
-        if self.cursor.fetchall():
-            return True
-        return False
-
     def get_user_by_name(self, name):
         with self.connection:
             if not self._check_user_existence_by_name(name):
@@ -43,18 +39,47 @@ class UserDatabase(Database):
             assert len(entries) == 1
             return User(*entries[0])
 
-    def create_user(self, name, salt, enc_dek):
+    def create_user(self, name, password):
+        salt = os.urandom(16)
+        kek = self._derive_key_encryption_key_from_password(password, salt)
+        dek = Fernet.generate_key()
+        fernet = Fernet(kek)
+        enc_dek = fernet.encrypt(dek)
+        user = User(name, base64.urlsafe_b64encode(salt).decode(), enc_dek.decode())
+        self.insert_user(user)
+            
+    def insert_user(self, user):
         with self.connection:
-            if self._check_user_existence_by_name(name):
+            if self._check_user_existence_by_name(user.name):
                 raise AlreadyExists
             self.cursor.execute('INSERT INTO users VALUES (:name, :salt, :enc_dek)',
-                                {'name': name, 'salt': salt, 'enc_dek': enc_dek})
+                                {'name': user.name, 'salt': user.salt, 'enc_dek': user.enc_dek})
 
     def remove_user_by_name(self, name):
         with self.connection:
             if not self._check_user_existence_by_name(name):
                 raise DoesNotExist
             self.cursor.execute('DELETE FROM users WHERE name = :name', {'name': name})
+
+    def _select_user_by_name(self, name):
+        self.cursor.execute('SELECT * FROM users WHERE name = :name', {'name': name})
+
+    def _check_user_existence_by_name(self, name):
+        self._select_user_by_name(name)
+        if self.cursor.fetchall():
+            return True
+        return False
+
+    @staticmethod
+    def _derive_key_encryption_key_from_password(password, salt):
+        key_derivation_function = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        return base64.urlsafe_b64encode(key_derivation_function.derive(password.encode()))
 
 
 class AlreadyExists(Exception):
