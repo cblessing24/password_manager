0ce9b89a5affcf7f6d10fb5c8cc01638fd884d0e
Christoph <chris24.blessing@gmail.com>
2018-09-08 12:38:58 +0200

    Validate password

--- validation.py
+++ validation.py
@@ -1,21 +1,34 @@
 import click
 
 
-def new_username(ctx, _param, value):
+def new_username(ctx, param, value):
     manager = ctx.obj['manager']
     while True:
         if not manager.check_user_existence_by_name(value):
             break
         click.echo(f'Error: A user with the username "{value}" already exists. Please choose a different name.')
-        value = click.prompt('Username', type=str)
+        value = click.prompt(param.prompt, type=str)
     return value
 
 
-def username(ctx, _param, value):
+def username(ctx, param, value):
     manager = ctx.obj['manager']
     while True:
         if manager.check_user_existence_by_name(value):
             break
         click.echo(f'Error: A user with the username "{value}" does not exist. Please choose a different name.')
-        value = click.prompt('Username', type=str)
+        value = click.prompt(param.prompt , type=str)
     return value
+
+
+def password(ctx, param, value):
+    manager = ctx.obj['manager']
+    username = ctx.params['username']
+    while True:
+        if manager.authenticate_user(username, value):
+            break
+        click.echo(f'Error: Incorrect password. Please try again.')
+        value = click.prompt(param.prompt, type=str, hide_input=True)
+    return value
+
+
