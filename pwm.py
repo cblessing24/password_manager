e711c262e068535a57681bb89411ee1795080996
Christoph <chris24.blessing@gmail.com>
2018-09-08 16:37:22 +0200

    Moved authentication to account group

--- pwm.py
+++ pwm.py
@@ -5,19 +5,13 @@ from classes import PasswordManager
 
 
 @click.group()
-def cli():
-    pass
-
-
-@cli.group()
 @click.pass_context
-def account(ctx):
-    """Manage your account."""
+def cli(ctx):
     manager = PasswordManager()
     ctx.obj = {'manager': manager}
 
 
-@account.command()
+@cli.command()
 @click.option('--username', type=str, help='Username for the new account.', prompt=True,
               callback=validation.new_username)
 @click.option('--password', type=str, help='Password for the new account.', prompt=True,
@@ -30,31 +24,40 @@ def new(ctx, username, password):
     click.echo(f'Success: Created a new user with the username "{username}".')
 
 
+@cli.group()
+@click.pass_context
+def account(ctx):
+    """Manage your account."""
+    manager = ctx.obj['manager']
+    while True:
+        username = click.prompt('Username', type=str)
+        if manager.check_user_existence_by_name(username):
+            break
+        click.echo(f'Error: A user with the username "{username}" does not exist.')
+    while True:
+        password = click.prompt('Password', type=str, hide_input=True)
+        if manager.authenticate_user(username, password):
+            break
+        click.echo(f'Error: Invalid password for user "{username}".')
+    ctx.obj.update({'username': username, 'password': password})
+
+
 @account.command()
-@click.option('--username', type=str, help='Your username.', prompt=True,
-              callback=validation.username)
-@click.option('--old_password', type=str, help='Your old password.', prompt=True, hide_input=True,
-              callback=validation.password)
 @click.option('--new_password', type=str, help='Your new password.', prompt=True, hide_input=True,
               callback=validation.new_password)
 @click.pass_context
-def change_password(ctx, username, old_password, new_password):
+def change_password(ctx, new_password):
     """Change your password."""
-    manager = ctx.obj['manager']
-    manager.change_user_password(username, old_password, new_password)
+    ctx.obj['manager'].change_user_password(ctx.obj['username'], ctx.obj['password'], new_password)
     click.echo('Success: Password changed.')
 
 
 @account.command()
-@click.option('--username', type=str, help='Your username.', prompt=True,
-              callback=validation.username)
-@click.option('--password', type=str, help='Your password.', prompt=True, hide_input=True,
-              callback=validation.password)
 @click.pass_context
-def delete(ctx, username, password):
+def delete(ctx):
     click.confirm('This action can not be reversed. Are you sure?', abort=True)
-    manager = ctx.obj['manager']
-    manager.delete_user(username)
+    username = ctx.obj['username']
+    ctx.obj['manager'].delete_user(username)
     click.echo(f'Success: Deleted the user with the username "{username}".')
 
 
