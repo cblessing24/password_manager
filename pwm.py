8b955942be71d3f29a5a16d9ef695c64f48f52c1
Christoph <chris24.blessing@gmail.com>
2018-09-08 16:07:41 +0200

    Change password command

--- pwm.py
+++ pwm.py
@@ -27,7 +27,22 @@ def new(ctx, username, password):
     """Create a new account."""
     manager = ctx.obj['manager']
     manager.create_user(username, password)
-    click.echo(f'Successfully created a new user with the username "{username}".')
+    click.echo(f'Success: Created a new user with the username "{username}".')
+
+
+@account.command()
+@click.option('--username', type=str, help='Your username.', prompt=True,
+              callback=validation.username)
+@click.option('--old_password', type=str, help='Your old password.', prompt=True, hide_input=True,
+              callback=validation.password)
+@click.option('--new_password', type=str, help='Your new password.', prompt=True, hide_input=True,
+              callback=validation.new_password)
+@click.pass_context
+def change_password(ctx, username, old_password, new_password):
+    """Change your password."""
+    manager = ctx.obj['manager']
+    manager.change_user_password(username, old_password, new_password)
+    click.echo('Success: Password changed.')
 
 
 def main():
