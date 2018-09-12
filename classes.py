6e2d5d6fbcafd383f2de9a0613a13be97f0270be
Christoph <chris24.blessing@gmail.com>
2018-09-12 12:55:24 +0200

    Started refactor

--- classes.py
+++ classes.py
@@ -1,138 +1,24 @@
-import os
-import sqlite3
-import base64
-from dataclasses import dataclass
+class Password:
 
-from cryptography.fernet import Fernet, InvalidToken
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    def __init__(self, name, info, password):
+        self.name = name
+        self.info = info
+        self.password = password
 
 
 class PasswordManager:
 
-    def __init__(self, directory=None):
-        self.user_database = UserDatabase(directory)
+    def __init__(self):
+        pass
 
-    def check_user_existence_by_name(self, name):
-        return name in self.user_database
+    def authenticate(self, master_password):
+        pass
 
-    def create_user(self, name, password):
-        salt = os.urandom(16)
-        kek = self._derive_key_encryption_key_from_password(password, salt)
-        dek = Fernet.generate_key()
-        fernet = Fernet(kek)
-        enc_dek = fernet.encrypt(dek)
-        user = User(name, self.decode_salt(salt), enc_dek.decode())
-        self.user_database.insert_user(user)
+    def get(self, name):
+        pass
 
-    def authenticate_user(self, name, password):
-        user = self.user_database.get_user_by_name(name)
-        salt = self.encode_salt(user.salt)
-        kek = self._derive_key_encryption_key_from_password(password, salt)
-        fernet = Fernet(kek)
-        try:
-            fernet.decrypt(user.enc_dek.encode())
-        except InvalidToken:
-            return False
-        return True
+    def new(self, name, info, password):
+        pass
 
-    def change_user_password(self, name, old_password, new_password):
-        user = self.user_database.get_user_by_name(name)
-        salt = self.encode_salt(user.salt)
-        old_kek = self._derive_key_encryption_key_from_password(old_password, salt)
-        new_kek = self._derive_key_encryption_key_from_password(new_password, salt)
-        old_fernet = Fernet(old_kek)
-        new_fernet = Fernet(new_kek)
-        dek = old_fernet.decrypt(user.enc_dek.encode())
-        new_enc_dek = new_fernet.encrypt(dek)
-        self.user_database.update_user_encrypted_data_encryption_key(name, new_enc_dek.decode())
-
-    def delete_user(self, name):
-        self.user_database.remove_user_by_name(name)
-
-    @staticmethod
-    def _derive_key_encryption_key_from_password(password, salt):
-        key_derivation_function = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=100000,
-            backend=default_backend()
-        )
-        return base64.urlsafe_b64encode(key_derivation_function.derive(password.encode()))
-
-    @staticmethod
-    def decode_salt(encoded_salt):
-        return base64.urlsafe_b64encode(encoded_salt).decode()
-
-    @staticmethod
-    def encode_salt(decoded_salt):
-        return base64.urlsafe_b64decode(decoded_salt.encode())
-
-
-class Database:
-
-    def __init__(self, directory):
-        if directory is None:
-            directory = '.'
-        self.connection = sqlite3.connect(os.path.join(directory, 'password_manager.db'))
-        self.cursor = self.connection.cursor()
-
-
-class UserDatabase(Database):
-
-    def __init__(self, directory):
-        super().__init__(directory)
-        with self.connection:
-            self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                name text,
-                salt text,
-                enc_dek text
-                )''')
-
-    def get_user_by_name(self, name):
-        with self.connection:
-            self._select_user_by_name(name)
-            entries = self.cursor.fetchall()
-            assert len(entries) == 1
-            return User(*entries[0])
-
-    def insert_user(self, user):
-        with self.connection:
-            self.cursor.execute('INSERT INTO users VALUES (:name, :salt, :enc_dek)',
-                                {'name': user.name, 'salt': user.salt, 'enc_dek': user.enc_dek})
-
-    def remove_user_by_name(self, name):
-        with self.connection:
-            self.cursor.execute('DELETE FROM users WHERE name = :name', {'name': name})
-
-    def update_user_encrypted_data_encryption_key(self, name, new_enc_dek):
-        with self.connection:
-            self.cursor.execute('''UPDATE users SET enc_dek = :new_enc_dek
-                WHERE name = :name''', {'new_enc_dek': new_enc_dek, 'name': name})
-
-    def _select_user_by_name(self, name):
-        self.cursor.execute('SELECT * FROM users WHERE name = :name', {'name': name})
-
-    def __contains__(self, name):
-        self._select_user_by_name(name)
-        if self.cursor.fetchall():
-            return True
-        return False
-
-
-@dataclass()
-class User:
-    name: str
-    salt: str
-    enc_dek: str
-
-
-def main():
-    manager = PasswordManager()
-    print(manager.authenticate_user('test', 'test'))
-
-
-if __name__ == '__main__':
-    main()
+    def delete(self, name):
+        pass
--- pwm.py
+++ pwm.py
@@ -1,71 +1,35 @@
 import click
 
-import validation
 from classes import PasswordManager
 
 
-def login_required(func):
-    func = click.option('--password', type=str, help='Your password.', prompt=True, hide_input=True,
-                        callback=validation.validate_password, expose_value=False)(func)
-    func = click.option('--username', type=str, help='Your username.', prompt=True,
-                        callback=validation.validate_username, expose_value=False)(func)
-    return func
-
-
 @click.group()
+@click.option('--master_password', type=str, prompt=True, hide_input=True)
 @click.pass_context
-def cli(ctx):
-    """A basic password manager."""
-    manager = PasswordManager()
-    ctx.obj = {'manager': manager}
+def cli(ctx, master_password):
+    ctx.obj = PasswordManager()
+    if not ctx.obj.authenticate(master_password):
+        ctx.abort()
 
 
-@cli.group()
+@cli.command()
+@click.option('--name', type=str, prompt=True)
 @click.pass_context
-def account(_ctx):
-    """Manage your account."""
-    pass
+def get(ctx, name):
+    ctx.obj.get(name)
 
 
-@account.command()
-@click.option('--username', type=str, help='Username for the new account.', prompt=True,
-              callback=validation.validate_new_username)
-@click.option('--password', type=str, help='Password for the new account.', prompt=True,
-              hide_input=True, confirmation_prompt=True)
+@cli.command()
+@click.option('--name', type=str, prompt=True)
+@click.option('--info', type=str, prompt=True)
+@click.option('--password', type=str, prompt=True)
 @click.pass_context
-def new(ctx, username, password):
-    """Create a new account."""
-    manager = ctx.obj['manager']
-    manager.create_user(username, password)
-    click.echo(f'Success: Created a new account with the username "{username}".')
+def new(ctx, name, info, password):
+    ctx.obj.new(name, info, password)
 
 
-@account.command()
-@login_required
-@click.option('--new_password', type=str, help='Your new password.', prompt=True, hide_input=True,
-              callback=validation.validate_new_password)
+@cli.command()
+@click.option('--name', type=str, prompt=True)
 @click.pass_context
-def change_password(ctx, new_password):
-    """Change the password of your account."""
-    ctx.obj['manager'].change_user_password(ctx.obj['username'], ctx.obj['password'], new_password)
-    click.echo('Success: Your password has been changed.')
-
-
-@account.command()
-@login_required
-@click.pass_context
-def delete(ctx):
-    """Delete your account."""
-    click.confirm('This action can not be reversed. Are you sure?', abort=True)
-    username = ctx.obj['username']
-    ctx.obj['manager'].delete_user(username)
-    click.echo('Success: Your account has been deleted.".')
-
-
-def main():
-    manager = PasswordManager()
-    print(manager.check_user_existence_by_name('Christoph'))
-
-
-if __name__ == '__main__':
-    main()
+def delete(ctx, name):
+    ctx.obj.delete(name)
