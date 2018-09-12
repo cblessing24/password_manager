be36ec9d09c550f6704a0a10d9548e8131481f3c
Christoph <chris24.blessing@gmail.com>
2018-09-12 16:53:19 +0200

    Added pyperclip to requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     install_requires=[
         'Click',
         'clipboard',
-        'cryptography'
+        'cryptography',
+        'pyperclip'
     ],
     entry_points='''
       [console_scripts]
