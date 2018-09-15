484aacde71ce04132453c3dc74289d95ecef1b44
Christoph <chris24.blessing@gmail.com>
2018-09-15 12:11:41 +0200

    Validate name.

--- validation.py
+++ validation.py
@@ -2,10 +2,19 @@ import click
 
 
 def validate_master_password(ctx, _param, value):
+    """Validates the master password."""
     if not ctx.obj.user_exists:
+        # Confirm master password if we want to create a new user.
         repeated_master_password = click.prompt(
             'Repeat for confirmation', type=str, hide_input=True)
         if repeated_master_password != value:
             ctx.fail('The two passwords do not match.')
     if not ctx.obj.authenticate(value):
         ctx.fail('Incorrect password.')
+
+
+def validate_name(ctx, _param, value):
+    """Validates the name."""
+    if value not in ctx.obj:
+        ctx.fail(f'A password with the name "{value}" does not exist.')
+    return value
